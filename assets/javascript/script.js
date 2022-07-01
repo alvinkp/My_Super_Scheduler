c5ab57aef8c2ba88a5471b44110bf38f59027be4
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-06-30 22:33:02 -0400

    Updated HTML and JS
    *HTML*
    
    - Added Favicon
    
    *JS*
    
    - Added 3 functions
    
    *OTHER*
    
    - Created images folder and added favicon.ico to it

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,27 @@
 // Variable Declarations
 var todaysDateContainer = document.querySelector("#currentDay");
+var dayStart = 9;
+var dayEnd = 5;
 
 // Set current Day, Month and ordinal Date in "currentDay" <p>
 todaysDateContainer.textContent = moment().format("dddd, MMMM Do");
 
+function returnNextHour(currentHour){
+    if(currentHour >= 12){
+        currentHour = 1;   
+    } else {
+        currentHour += 1;
+    }
+    return currentHour;
+}
+
+function returnTotalWorkHours(start, end){
+    var myHours = Math.abs(24 - (start + end));
+    return myHours;
+}
+
+function createMyScheduleLayout(myStartTime){
+    for(var i = 0; i < returnTotalWorkHours(); i++) {
+        
+    }
+}
