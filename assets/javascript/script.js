dc9599fe9df387bf0274f49edfcf06ccf361b5a2
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-07-01 16:20:18 -0400

    Updated CSS & JS
    *CSS*
    
    - Adjusted the left and right margin on the .row selector
    
    - Turned the .hour selector into a flexbox and centered the text
    
    *JS*
    
    - Added createMyScheduleLayout() to add time blocks to the html container.
    
    - Added createMyTimeBlocks() to create the elements for the timeblocks and append them to a div.
    
    *OTHER*
    
    - Added an image for use on the save button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,47 @@
 // Variable Declarations
 var todaysDateContainer = document.querySelector("#currentDay");
-var dayStart = 9;
-var dayEnd = 5;
+var timeBlockContainer = document.querySelector(".container");
 
 // Set current Day, Month and ordinal Date in "currentDay" <p>
 todaysDateContainer.textContent = moment().format("dddd, MMMM Do");
 
-function returnNextHour(currentHour){
-    if(currentHour >= 12){
-        currentHour = 1;   
-    } else {
-        currentHour += 1;
-    }
-    return currentHour;
+// Returns a bootstrap element that takes in text and has a button to save inputted text to 
+function createMyTimeBlocks(time){
+    var myBSElement = document.createElement("div");
+    var myTimeElement = document.createElement("p");
+    var myInputElement = document.createElement("input");
+    var mySaveButton = document.createElement("button");
+
+    //Assign relevant properties to the Div
+    myBSElement.setAttribute("class", "input-group mb-3");
+
+    //Assign time to paragraph tag
+    myTimeElement.setAttribute("class", "hour");
+    myTimeElement.textContent = time;
+    myBSElement.appendChild(myTimeElement);
+
+    //Assign relevant properties to the Input and append to div
+    myInputElement.setAttribute("type", "text");
+    myInputElement.setAttribute("class", "form-control row");
+    myBSElement.appendChild(myInputElement);
+
+    //Assign relevant properties to the Save Button and append to div
+    mySaveButton.setAttribute("class", "btn btn-outline-secondary saveBtn");
+    mySaveButton.setAttribute("type", "button");
+    mySaveButton.setAttribute("id", "button-addon2");
+    mySaveButton.appendChild(document.createElement("img"));
+    mySaveButton.lastElementChild.setAttribute("src", "./assets/images/Save_icon.png");
+    myBSElement.appendChild(mySaveButton);
+
+    return myBSElement;
 }
 
-function returnTotalWorkHours(start, end){
-    var myHours = Math.abs(24 - (start + end));
-    return myHours;
-}
-
-function createMyScheduleLayout(myStartTime){
-    for(var i = 0; i < returnTotalWorkHours(); i++) {
-        
+// createMySchedule Function - populates the timeBlockContainer with timeblocks
+function createMyScheduleLayout(){
+    for(var i = 0; i < 9; i++) {
+        var MyTimeModifier = i+9;
+        timeBlockContainer.appendChild(createMyTimeBlocks(moment({hour: MyTimeModifier}).format("h A")));
     }
 }
+
+createMyScheduleLayout();
